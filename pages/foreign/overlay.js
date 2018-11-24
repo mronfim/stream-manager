31f93a5e673daba16c4c7d347d9badce852051a5
Matheus Ronfim <mronfim@gmail.com>
2018-11-24 11:15:41 -0500

    Fix import error

--- pages/foreign/overlay.js
+++ pages/foreign/overlay.js
@@ -3,45 +3,45 @@ import io from 'socket.io-client'
 
 import stylesheet from '../../styles/foreign/overlay.scss'
 
-import FollowerGoal from '../components/FollowerGoal'
+// import FollowerGoal from '../components/FollowerGoal'
 
 class Overlay extends React.Component {
-    constructor(props) {
-        super(props)
-        this.socket = io('http://localhost:3000')
-        this.state = {
-            user: {}
-        }
-    }
+    // constructor(props) {
+    //     super(props)
+    //     this.socket = io('http://localhost:3000')
+    //     this.state = {
+    //         user: {}
+    //     }
+    // }
 
-    componentDidMount() {
-        this.socket.on('user info', data => {
-            this.setState({
-                ...this.state,
-                user: data,
-            })
-        })
+    // componentDidMount() {
+    //     this.socket.on('user info', data => {
+    //         this.setState({
+    //             ...this.state,
+    //             user: data,
+    //         })
+    //     })
 
-        this.socket.on('new follow', data => {
-            let user = this.state.user || {}
-            let followers = user.followers || 0
+    //     this.socket.on('new follow', data => {
+    //         let user = this.state.user || {}
+    //         let followers = user.followers || 0
 
-            this.setState({
-                ...this.state,
-                user: {
-                    ...user,
-                    followers: followers + data.total,
-                    recentFollower: data.recentFollower,
-                }
-            })
-        })
-    }
+    //         this.setState({
+    //             ...this.state,
+    //             user: {
+    //                 ...user,
+    //                 followers: followers + data.total,
+    //                 recentFollower: data.recentFollower,
+    //             }
+    //         })
+    //     })
+    // }
 
     render() {
         return (
             <div className="overlay-container">
                 <style dangerouslySetInnerHTML={{ __html: stylesheet }} />
-                <FollowerGoal />
+                {/* <FollowerGoal /> */}
             </div>
         )
     }
